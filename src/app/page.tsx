2b0c4ac1a882c80a7bb2642aea874cb6f77a8268
Augustine Agladima <austinagladima@gmail.com>
2025-11-13 16:57:42 +0100

    hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,7 +157,7 @@ export default function Home() {
 
         <button
           onClick={() => setShowDashboard(true)}
-          className="mt-10 px-6 py-3 bg-[#0060B6] text-white rounded-full text-lg hover:bg-green-700 transition"
+          className="mt-10 px-6 py-3 bg-[#0060B6] text-white rounded-full text-lg hover:bg-blue-700 transition"
         >
           Continue to Dashboard
         </button>
